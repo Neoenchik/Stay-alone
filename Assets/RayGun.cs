decce9cf6ff19876ab8938b4cfaaf8b17bb689c3
Masud11 <107207594+Masudhaji2002@users.noreply.github.com>
2023-01-11 09:38:59 +0300

    full my project vs

---
+++ Assets/RayGun.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using static UnityEngine.Rendering.SplashScreen;
+using UnityEngine;
+
+public class RayGun : MonoBehaviour
+{
+    public float shootRate;
+    private float m_shootRateTimeStamp;
+
+    public GameObject m_shotPrefab;
+
+    RaycastHit hit;
+    float range = 100.0f;
+    AudioSource m_shootingSound;
+
+    private void Start()
+    {
+        m_shootingSound = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+
+        if (Input.GetMouseButton(0))
+        {
+            if (Time.time > m_shootRateTimeStamp)
+            {
+                m_shootingSound.Play();
+                shootRay();
+                m_shootRateTimeStamp = Time.time + shootRate;
+            }
+        }
+
+    }
+
+    void shootRay()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit, range))
+        {
+            GameObject laser = GameObject.Instantiate(m_shotPrefab, transform.position, transform.rotation) as GameObject;
+            laser.GetComponent<ShotBehavior>().setTarget(hit.point);
+            GameObject.Destroy(laser, 0.5f);
+
+
+        }
+
+    }
+
+
+
+}
---
+++ Assets/WeaponsSwitcher.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponSwitcher : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            switchWeapons();
+        }
+
+    }
+
+    void switchWeapons()
+    {
+        foreach (Transform weapon in transform)
+        {
+            weapon.gameObject.SetActive(!weapon.gameObject.activeSelf);
+        }
+    }
+}
