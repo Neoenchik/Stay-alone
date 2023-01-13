7169ca614d062bdf81164539783d0f3d5bbbf864
Neoenchik <102302057+Neoenchik@users.noreply.github.com>
2023-01-13 05:12:48 +0300

    Подитог

---
+++ Assets/Scripts/Indicators.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Indicators : MonoBehaviour
+{
+    
+    public Image healthBar, foodBar, energyBar;
+    [Header("Set in Inspector")]
+    public float healthAmount = 100;
+    public float foodAmount = 100;
+    public float energyAmount = 100;
+    public float secondsToEmptyHealth = 300f;
+    public float secondsToEmptyFood = 120f;
+
+    void Start()
+    {
+        healthBar.fillAmount = healthAmount / 100;
+        foodBar.fillAmount = foodAmount / 100;
+    }
+
+
+    void Update()
+    {
+        if(foodAmount > 0)
+        {
+            foodAmount -= 100 / secondsToEmptyFood * Time.deltaTime;
+            foodBar.fillAmount = foodAmount / 100;
+        }
+
+        if(foodAmount <= 0 )
+        {
+            healthAmount -= 100 / secondsToEmptyHealth * Time.deltaTime;
+        }
+        healthBar.fillAmount = healthAmount / 100;
+    }
+}
