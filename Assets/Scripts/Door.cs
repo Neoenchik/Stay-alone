d53e8907b79bb662538ec6fb0655399fdc478e61
Neoenchik <102302057+Neoenchik@users.noreply.github.com>
2023-01-15 05:00:32 +0300

    Doors
    Сделана подробная анимация открытия дверей

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -2,14 +2,23 @@ using UnityEngine;
 
 public class Door : MonoBehaviour, IInteractable
 {
-    public Animator m_Animator;
+    //private Animator[] m_Animator = new Animator(4);
+    public Animator m_Animator1,
+        m_Animator2,
+        m_Animator3,
+        m_Animator4,
+        m_Animator5;
     public bool IsOpen;
 
     void Start()
     {
         if (IsOpen)
         {
-            m_Animator.SetBool("IsOpen", true);
+            m_Animator1.SetBool("IsOpen", true);
+            m_Animator2.SetBool("IsOpen", true);
+            m_Animator3.SetBool("IsOpen", true);
+            m_Animator4.SetBool("IsOpen", true);
+            m_Animator5.SetBool("IsOpen", true);
         }
     }
 
@@ -24,12 +33,20 @@ public class Door : MonoBehaviour, IInteractable
         IsOpen = !IsOpen;
         if (IsOpen)
         {
-            m_Animator.SetBool("IsOpen", true);
+            m_Animator1.SetBool("IsOpen", true);
+            m_Animator2.SetBool("IsOpen", true);
+            m_Animator3.SetBool("IsOpen", true);
+            m_Animator4.SetBool("IsOpen", true);
+            m_Animator5.SetBool("IsOpen", true);
             this.GetComponent<BoxCollider>().isTrigger = false;
         }
         else
         {
-            m_Animator.SetBool("IsOpen", false);
+            m_Animator1.SetBool("IsOpen", false);
+            m_Animator2.SetBool("IsOpen", false);
+            m_Animator3.SetBool("IsOpen", false);
+            m_Animator4.SetBool("IsOpen", false);
+            m_Animator5.SetBool("IsOpen", false);
             this.GetComponent<BoxCollider>().isTrigger = true;
         }
     }
